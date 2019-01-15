f36ff29212545b23eabae09537760c0f02dd97d0
Kostiantyn Koval <kostiantyn.koval@gmail.com>
2019-01-15 18:21:44 +0200

    dashboard started

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,20 +1,73 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import {withStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
+const sections = ['Applied', 'Interviewing', 'Hired'];
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    display: 'flex',
+    height: 140,
+    width: '30vw',
+    paddingTop: 8,
+    justifyContent: 'center'
+  },
+  control: {
+    padding: theme.spacing.unit * 2,
+  },
+});
 
 class Dashboard extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      applied: [],
+      interviewing: [],
+      hired: []
+    }
+  }
+
+
 
   async componentDidMount() {
     const data = await fetch('https://randomuser.me/api/?nat=gb&results=5', {method: 'get'});
     const users = await data.json();
-    const appliedUsers =  users.results.map(user => {
+    const appliedUsers = users.results.map(user => {
       user.status = 'applied';
       return user;
     });
-    console.log('appliedUsers', appliedUsers)
+    this.setState({hired: appliedUsers})
+
   }
 
+
+
   render() {
-    return <h1>Hello!</h1>
+    const { classes } = this.props;
+    return (
+      <Grid container className={classes.root} spacing={16}>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={Number(16)}>
+            {sections.map(value => (
+              <Grid key={value} item>
+                <Paper className={classes.paper}>
+                  <Typography variant="h5" component="h3">
+                    {value}
+                  </Typography>
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    )
   }
 }
 
-export default Dashboard
+export default withStyles(styles)(Dashboard)
